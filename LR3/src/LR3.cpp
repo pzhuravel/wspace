56aa096ac7fa328a0b90079d9b60ecabada92fc0
pzhuravel <pzhuravel@gmail.com>
2015-03-29 18:42:00 +0300

    29/03/2015

---
+++ LR3/src/LR3.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : LR3.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	float sum = 0;
+	for(int i=1;i<=10;i++)
+	{
+		sum+=i;
+	}
+	printf("Sum is %3.2f",sum);
+	return 0;
+}
